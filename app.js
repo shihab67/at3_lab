daf61873587805d6d858b26cb692af773d20145f
shihab67 <51674775+shihab67@users.noreply.github.com>
2019-10-23 12:31:08 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+//DECLARATION
+var express = require('express');
+var bodyParser = require('body-parser');
+var expSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
+var login = require('./controllers/login');
+var home = require('./controllers/home');
+var user = require('./controllers/user');
+var logout = require('./controllers/logout');
+var app = express();
+
+
+//CONFIGURATION
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(expSession({secret:'my top secret value', saveUninitialized:true, resave: false}));
+app.use(cookieParser());
+app.use('/abc', express.static('xyz'))
+app.use('/login', login);
+app.use('/home', home);
+app.use('/user', user);
+app.use('/logout', logout);
+
+
+//ROUTER
+app.get('/', function(request, response){
+	response.send('index page!');
+});
+
+app.get('/test/your/:name/:id', function(request, response){
+	var id = request.params.id;
+	var name = request.params.name;
+	response.send(id+" "+name);
+
+});
+
+
+//SERVER STARTUP
+app.listen(3000, function(){
+	console.log('server started at 3000...');
+});
